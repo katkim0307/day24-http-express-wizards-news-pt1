23ea5ad69fbff415f7db930e0343fa3d13ff3aa0
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-09 13:41:26 -0400

    added a single-post route /posts/id#

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-
 app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
@@ -42,5 +41,34 @@ app.get("/", (req, res) => {
   // send a response
   res.send(html);
 });
+
+// ADDING DYNAMIC ROUTING with PARAMETERS
+// localhost:1337/posts/1
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>${post.title}</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png" />Wizard News</header>
+      <div class="news-item">
+        <p>
+          ${post.title}
+          <small>(by ${post.name})</small>
+        </p>
+        <p>
+          ${post.content}
+        </p>
+      </div>
+    </div>
+  </body>
+  </html>`
+  res.send(html);
+})
 
 app.listen(1337);
