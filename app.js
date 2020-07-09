14a2acd0ace320e673ac9cf7e24dc552d63f4c8a
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-09 12:09:02 -0400

    listing posts

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  // get the list of posts
+  const posts = postBank.list();
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  // prepare some html to send the list as output
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${post.title}</li>`)}
+    </ul>
+  </body>
+  </html>`;
+
+  // send a response
+  res.send(html);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
